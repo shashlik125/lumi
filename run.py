dc9f6d85baf5407e932aa90177aa3fd570cf9f30
Анастасия <mysorka1234nasta@mail.ru>
2026-03-17 13:35:11 +0300

    переделка

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     # Используем порт из переменной окружения или 5000 по умолчанию
     port = int(os.environ.get('PORT', 5000))
     print(f"🚀 Запуск приложения Lumi на порту {port}...")
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
